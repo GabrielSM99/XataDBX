b044d259a4f4c9d67288da7ec48df58895e59b25
GabrielSD99 <gabrielsd99@hotmail.com>
2024-11-08 11:02:52 -0300

    Commit X

---
+++ from xata.py
@@ -0,0 +1,2 @@
+from xata.client import XataClient
+client = XataClient(api_key="xau_aWHcflbqo8ucXAA9IDKrw0JlDkGbsOrG0", db_url="https://Gabriel-Moura-s-workspace-a5ttv7.us-east-1.xata.sh/db/BDPJT1")
